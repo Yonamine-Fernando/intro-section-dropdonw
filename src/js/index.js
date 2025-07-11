f70b735decc2d436c4feec0efabb99a5ef87d4b4
Fernando-Yonamine <yonamine.fernando@hotmail.com>
2025-07-11 19:01:05 +0900

    project done

---
+++ src/js/index.js
@@ -0,0 +1,18 @@
+const checkedBox = document.querySelector("#menu-hamburguer");
+const menu = document.querySelector("#menus");
+const overley = document.querySelector(".overlay")
+
+console.log(menu);
+
+
+checkedBox.addEventListener("change", () => {
+    if (checkedBox.checked) {
+        menu.classList.add
+        ("hidden");
+        overley.classList.add("hidden")
+    } else {
+        menu.classList.remove("hidden");
+         overley.classList.remove("hidden");
+    }
+
+});
