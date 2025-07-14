9a565d299ecdb05668dac4c7b21f5ad8de7cc0f3
Fernando-Yonamine <yonamine.fernando@hotmail.com>
2025-07-14 16:24:48 +0900

    refatorado html, css

--- src/js/index.js
+++ src/js/index.js
@@ -1,17 +1,21 @@
 const checkedBox = document.querySelector("#menu-hamburguer");
 const menu = document.querySelector("#menus");
-const overley = document.querySelector(".overlay")
-
-console.log(menu);
+const overlay = document.querySelector(".overlay")
 
 
 checkedBox.addEventListener("change", () => {
     if (checkedBox.checked) {
         menu.classList.add("hidden");
-        overley.classList.add("hidden")
+        overlay.classList.add("hidden")
     } else {
         menu.classList.remove("hidden");
-         overley.classList.remove("hidden");
+         overlay.classList.remove("hidden");
     }
 
+});
+
+overlay.addEventListener("click", () => {
+  checkedBox.checked = false;
+  menu.classList.remove("hidden");
+  overlay.classList.remove("hidden");
 });
